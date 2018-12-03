08ff9b83e53c52f8cdd6af5bee1151eac2f5cc4c
bever <bever@192.168.1.68>
2018-12-03 17:00:17 +1300

    Hello World print

--- JavaProgrammingBasics/src/javaprogrammingbasics/FirstProject.java
+++ JavaProgrammingBasics/src/javaprogrammingbasics/FirstProject.java
@@ -14,6 +14,8 @@ public class FirstProject {
     /**
      * @param args the command line arguments
      */
+    
+    //main is the point of entry for the program
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World!");
