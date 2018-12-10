4b223d3fdc978e4c0db224087d8e6492e2311bab
bever <bever@192.168.1.68>
2018-12-10 20:30:47 +1300

    Methods

--- JavaProgrammingBasics/src/javaprogrammingbasics/Methods.java
+++ JavaProgrammingBasics/src/javaprogrammingbasics/Methods.java
@@ -15,13 +15,23 @@ public class Methods {
         sayHelloWorld();
         
         sayHelloTo("Charilie");
+        
+        int x = returnFive();
+        System.out.println(x);
     }
     
+    // this method will say hello to whatever name is passed when called
     static void sayHelloTo(String name) {
         System.out.println("Hello, " + name);
     }
     
+    // this method simply says "Hello World!"
     static void sayHelloWorld() {
         System.out.println("Hello World!");
+    }
+    
+    // this method returns an int type with the value 5
+    static int returnFive() {
+        return 5;
     }
 }
